837f90d26d9c820f15f1de4bfb73832d045a9370
Jonathan Drake <jdrake@narrativescience.com>
2021-03-17 10:12:42 -0700

    tests

--- tests/test_process_events.py
+++ tests/test_process_events.py
@@ -36,3 +36,48 @@ def main(data):
             visitor.state_machine_visitors["main"].event_processor["processor"],
             "TestEventProcessor",
         )
+
+    def test_process_events__multiple_state_machines(self):
+        """Should parse a processor used by multiple state machines"""
+        code = """
+class TestEventProcessor(EventProcessor):
+    pass
+
+@process_events(processor=TestEventProcessor)
+def one(data):
+    pass
+
+@process_events(processor=TestEventProcessor)
+def two(data):
+    pass
+"""
+        visitor = parse_project_tree(ast.parse(code))
+        self.assertIn("TestEventProcessor", visitor.event_processor_visitors)
+        self.assertEqual(
+            visitor.state_machine_visitors["one"].event_processor["processor"],
+            "TestEventProcessor",
+        )
+        self.assertEqual(
+            visitor.state_machine_visitors["two"].event_processor["processor"],
+            "TestEventProcessor",
+        )
+
+    def test_process_events__multiple_default(self):
+        """Should parse multiple uses of the default processor"""
+        code = """
+@process_events()
+def one(data):
+    pass
+
+@process_events()
+def two(data):
+    pass
+"""
+        visitor = parse_project_tree(ast.parse(code))
+        self.assertDictEqual(visitor.event_processor_visitors, {})
+        self.assertIsNone(
+            visitor.state_machine_visitors["one"].event_processor["processor"]
+        )
+        self.assertIsNone(
+            visitor.state_machine_visitors["two"].event_processor["processor"]
+        )
